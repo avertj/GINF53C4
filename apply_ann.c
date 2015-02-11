88f9c52f1906348f6de331565e2e3b031df81db4
avertj <avert.julien@gmail.com>
2015-02-11 09:31:49 +0100

    Simplification de apply_ann

--- apply_ann.c
+++ apply_ann.c
@@ -6,45 +6,36 @@
 // arg 2 : annotation file
 int main(int argc, char *argv[])
 {
-    FILE *svm;
-    FILE *ann;
     char *command;
+    FILE *files[2];
 
-    if (strstr(argv[1], "http://") == argv[1]) {
-        asprintf(&command, "wget -O - \"%s\" -o /dev/null", argv[1]);
-    } else {
-        asprintf(&command, "cat \"%s\"", argv[1]);
-    }
-    if ((svm = popen(command, "r")) == NULL) {
-        fprintf(stderr,"Can't open svm file from \"%s\" command, exiting", command);
-        exit(1);
-    }
-
-    if (strstr(argv[2], "http://") == argv[2]) {
-        asprintf(&command, "wget -O - \"%s\" -o /dev/null", argv[2]);
-    } else {
-        asprintf(&command, "cat \"%s\"", argv[2]);
-    }
-    if ((ann = popen(command, "r")) == NULL) {
-        fprintf(stderr,"Can't open ann file from \"%s\" command, exiting", command);
-        exit(1);
+    int i;
+    for(i = 1; i < 3; i++) {
+        if (strstr(argv[i], "http://") == argv[i]) {
+            asprintf(&command, "wget -O - \"%s\" -o /dev/null", argv[i]);
+        } else {
+            asprintf(&command, "cat \"%s\"", argv[i]);
+        }
+        if ((files[i-1] = popen(command, "r")) == NULL) {
+            fprintf(stderr,"Can't open svm file from \"%s\" command, exiting", command);
+            exit(1);
+        }
     }
 
     free(command);
 
     char * svmline = NULL;
     size_t svmlen = 0;
-    ssize_t svmread;
     char * annline = NULL;
     size_t annlen = 0;
-    ssize_t annread;
 
-    while ((svmread = getline(&svmline, &svmlen, svm)) != -1 &&
-            (annread = getline(&annline, &annlen, ann)) != -1) {
+    while (getline(&svmline, &svmlen, files[0]) != -1 &&
+           getline(&annline, &annlen, files[1]) != -1) {
         int iann;
         sscanf(annline, "%*s %i\n", &iann);
 
-        if (iann)
+        if (iann) {
             printf("%d %s", iann, (svmline + 2));
+        }
     }
 }
